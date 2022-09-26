a4f786357aaa8243be811c1151a3b802b3c88459
joaonogueira0 <joaoguinogueira04@gmail.com>
2022-09-26 00:32:06 -0300

    Backup

--- JogoDaVelha/LibTicTacToe.h
+++ JogoDaVelha/LibTicTacToe.h
@@ -38,21 +38,6 @@ int VerificationD2();
 void FillGame();
 void imprimeJogo();
 
-int main(int argc, char *argv[])
-{
-
-    FillGame();
-    glutInit(&argc, argv);
-    glutInitWindowSize(500,500);
-    glutInitWindowPosition(600,200);
-    glutCreateWindow("Tic Tac Toe");
-    glutMouseFunc(mouse);
-    glutDisplayFunc(Draw);
-    glClearColor(0,0,0,0);
-    glutMainLoop();
-
-    return 0;
-}
 void Layout()
 {
     GLfloat x = 0.25;
